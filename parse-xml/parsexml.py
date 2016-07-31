4d9beab017cfffc91f50eb685a7058fe97d7a1bc
bibo <bibo19842003@gmail.com>
2016-07-31 23:57:46 +0800

    Add parse xml attribute

--- parse-xml/parsexml.py
+++ parse-xml/parsexml.py
@@ -6,10 +6,28 @@ import os
 default_xml = "./default.xml"
 dom = xml.dom.minidom.parse(default_xml)
 manifest = dom.documentElement
+
+defaultproject = manifest.getElementsByTagName("default")[0]
+defaultrevision = defaultproject.getAttribute("revision")
+defaultremote = defaultproject.getAttribute("remote")
+
 projects = manifest.getElementsByTagName("project")
 for project in projects:
-    path = project.getAttribute("path")
     name = project.getAttribute("name")
-#    print project
-#    print path, name
-    print "%s:%s" % (name, path)
+
+    if project.getAttribute("path"):
+        path = project.getAttribute("path")
+    else:
+        path = name
+
+    if project.getAttribute("revision"):
+        newrevision = project.getAttribute("revision")
+    else:
+        newrevision = defaultrevision
+
+    if project.getAttribute("origin"):
+        newremorte = project.getAttribute("origin")
+    else:
+        newremote = defaultremote
+
+    print "%s:%s:%s:%s" % (name, path, newrevision, newremote)
